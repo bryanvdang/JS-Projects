e2c032f9c5c40bbba6b796a617a5afa7b0fabb69
bryanvdang <bryanvdang@gmail.com>
2016-05-07 09:38:24 -0700

    updated

--- Simple Guessing Game/numberGuess.js
+++ Simple Guessing Game/numberGuess.js
@@ -1,16 +1,41 @@
 var number = 7;
 console.log("Hello");
-var input = Number(prompt("Guess a Number")); //user input from prompt gets stored into "input"
+var inputString = (prompt("Guess a Number")); //user input from prompt gets stored into "input"
+var guess =  Number(inputString);
+//converts the "input" to a Number
 
-if (input === number){   //converts the "input" to a Number
+//if user guesses correct number
+if (guess === number){   
 	alert("Good Job! That's Correct");
 }
-else if(input < number){
+//if input is too low
+else if(guess < number){
 	alert("The number is bigger");
 }
-else if(input > number){
+//if the input is too high
+else if(guess > number){
 	alert("The number is smaller");
 }
+//if not given a proper input
 else{
 	alert("invalid Input");
 }
+
+
+// while(inputString != 7){
+// 	do{
+// 		guess = prompt("Guess again");
+// 		//if input is too low
+// 		if(guess < number){
+// 			alert("The number is bigger");
+// 		}
+// 		//if the input is too high
+// 		else if(guess > number){
+// 			alert("The number is smaller");
+// 		}
+// 		//if not given a proper input
+// 		else{
+// 			alert("invalid Input");
+// 		}
+// 	}
+//}
