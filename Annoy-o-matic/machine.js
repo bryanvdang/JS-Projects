81cd10c813f761bb60cd555b87fa71c768705733
bryanvdang <bryanvdang@gmail.com>
2016-05-08 23:52:44 -0700

    modification?

--- Annoy-o-matic/machine.js
+++ Annoy-o-matic/machine.js
@@ -1,6 +1,6 @@
 var input = prompt("Are we there yet");
 
-while(input !== "yes")
+while(input.indexOf("yes") === -1) //while the string "yes" does not exist(-1) in the input
 {
 	input = prompt("Are we there yet?");
 }
