e7ff4ffcac0907092e1c6216eb73da7a122a1be7
bryanvdang <bryanvdang@gmail.com>
2016-05-10 02:09:47 -0700

    update files and new practice

--- To-do List/list.js
+++ To-do List/list.js
@@ -4,21 +4,43 @@ var input = prompt("What would you like to do?");
 
 while(input !== "quit")
 {
-	//if user types "new"
-	if(input === "new")
+	if(input === "new")	//if user types "new"
 	{
-		//new input gets stored in back of array
-		newInput = prompt("Enter a new todo");
-		//store value into new array
-		inputArray.push(newInput);
+		newTodo();
+	}
+	else if(input === "delete")
+	{
+		deleteTodo();
 	}
 	//if user types "list"
 	else if(input === "list")
 	{
-	console.log(inputArray);
+		list();
 	}
 //keep prompting user for input
 var input = prompt("What would you like to do?");
 }
 	//when user has typed "quit"
 	prompt("OK, you quit the app");
+
+function newTodo(){
+	//new input gets stored in back of array
+	newInput = prompt("Enter a new todo");
+	//store value into new array
+	inputArray.push(newInput);
+	console.log("Added To-do");
+}
+function deleteTodo(){
+	//ask for index of which To-do to delete
+	deleteInput = prompt("Enter index of which To-Do to delete?")
+		
+	inputArray.splice(deleteInput,1);
+	//name of array, where i'm going to cut, how far do i want to cut, just 1 index
+	console.log("Deleted To-do");
+}
+function list(){
+	inputArray.forEach(function(todo, i){ //todo holds each value for every item in that array and will be printed
+		// 'i' referrs to every index ^ that is passed in
+			console.log(i + ": " + todo);
+		});
+}
---
+++ functions Practice/iteration.js
@@ -0,0 +1,8 @@
+
+var numbers = [1,2,3,4,5,6,7,8,9,10];
+
+for(var i = 0; i < numbers.length; i++){
+	if(i% 3 === 0){
+	console.log(i);
+	}
+}
