d51415be2b31bfd41caa34a7b159207523821324
bryanvdang <bryanvdang@gmail.com>
2016-05-13 06:34:28 -0700

    modified files and added a new css file

--- Score Keeper/scoreKeeper.js
+++ Score Keeper/scoreKeeper.js
@@ -1,7 +1,10 @@
 var p1Button = document.getElementById("p1");
 var p2Button = document.getElementById("p2");
+var resetButton = document.getElementById("reset");
 var p1Display = document.querySelector("#p1Display");
 var p2Display = document.querySelector("#p2Display");
+var numInput =  document.querySelector("input");
+var playTo = document.querySelector("p span");
 var p1Score = 0;
 var p2Score = 0;
 var gameOver = false; //second stage of the game
@@ -13,6 +16,7 @@ p1Button.addEventListener("click", function(){
 		p1Score++;
 		if(p1Score === winningScore)
 		{
+			p1Display.classList.add("winner"); //calls the class from css file and changes text to green
 			gameOver = true; //prevents the user from adding after game is over cuz the rest of the code doesn't run
 		}
 		p1Display.textContent = p1Score;
@@ -24,8 +28,28 @@ p2Button.addEventListener("click", function(){
 		p2Score++;
 		if(p2Score === winningScore)
 		{
+			p2Display.classList.add("winner");
 			gameOver = true;
 		}
 	p2Display.textContent = p2Score;
 	}
+});
+resetButton.addEventListener("click", function(){
+	//WHEN USER CLICKS RESET
+	reset();
+});
+function reset()
+{
+	p1Score = 0;
+	p2Score = 0;
+	p1Display.textContent = p1Score;
+	p2Display.textContent = p2Score;
+	p1Display.classList.remove("winner"); //removes css class association
+	p2Display.classList.remove("winner");
+	gameOver = false; //enable us to keep playing
+}
+numInput.addEventListener("change", function(){ //change is if any value changes
+	playTo.textContent = this.value; //"this" instead of "numInput"
+	winningScore = Number(this.value);
+	reset();
 });
